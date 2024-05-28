01afd1bc8776a23e4a147de5aef381ea591f1de0
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2024-05-28 11:02:41 +0530

    Initial commit

---
+++ run-yolo1.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+model = YOLO('Yolo-weights/yolov8l.pt')
+image = cv2.imread('images/img2.jpg')
+# Scale the image to 50% of its original size
+scaled_image = cv2.resize(image, (0, 0), fx=0.3, fy=0.3)
+results = model(scaled_image, show=True)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
